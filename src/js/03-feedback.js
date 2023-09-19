12948fc9ffaf6a688aa975f74a398f886010d0bf
Olya Vorontsova <16olya1996@gmail.com>
2023-09-19 22:43:56 +0200

    add function saveFormState

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,15 @@
 import throttle from "lodash.throttle";
 
-const form = document.querySelector(".feedback-form");
-const email = document.querySelector('input[name="email"]');
-const message = document.querySelector('textarea[name="message"]');
+const feedbackForm = document.querySelector(".feedback-form");
+const emailInput = document.querySelector('input[name="email"]');
+const messageTextarea = document.querySelector('textarea[name="message"]');
 const LC_KEY = 'feedback-form-state';
 
+function saveFormState {
+    const formData = {
+        email: emailInput.value,
+        message: messageTextarea.value,
+    };
+    localStorage.setItem(LC_KEY, JSON.stringify(formData));
+}
 
