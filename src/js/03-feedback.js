9c6e10720d2e9a5eb1dc53aaa376f907ab853b26
Olya Vorontsova <16olya1996@gmail.com>
2023-09-19 22:29:46 +0200

    find el and add lc

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,8 @@
+import throttle from "lodash.throttle";
+
+const form = document.querySelector(".feedback-form");
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const LC_KEY = 'feedback-form-state';
+
 
