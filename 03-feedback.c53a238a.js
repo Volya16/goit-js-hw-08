329158d3545cc06d37ec7429aa71f2b482681c06
Volya16 <120327659+Volya16@users.noreply.github.com>
2023-09-20 09:08:48 +0000

    Deploying to gh-pages from @ Volya16/goit-js-hw-08@577b17f86f2c511cc10237cad1c8909d1c8b0b5c 🚀

--- 03-feedback.c53a238a.js
+++
@@ -1,3 +0,0 @@
-var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},t="object"==typeof e&&e&&e.Object===Object&&e,o="object"==typeof self&&self&&self.Object===Object&&self;t||o||Function("return this")(),Object.prototype.toString;document.querySelector(".feedback-form"),document.querySelector('input[name="email"]'),document.querySelector('textarea[name="message"]');//# sourceMappingURL=03-feedback.c53a238a.js.map
-
-//# sourceMappingURL=03-feedback.c53a238a.js.map
