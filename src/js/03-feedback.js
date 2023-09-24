90654160c75fbe9c08fd12bd08410c494ade38d3
Olya Vorontsova <16olya1996@gmail.com>
2023-09-24 23:01:37 +0200

    input clear and add throttle

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,30 +1,53 @@
-// import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
-const feedbackForm = document.querySelector(".feedback-form");
+const feedbackForm = document.querySelector('.feedback-form');
 const emailInput = document.querySelector('input[name="email"]');
 const messageTextarea = document.querySelector('textarea[name="message"]');
 const LC_KEY = 'feedback-form-state';
 
 // Save data to local storage
-function saveFormState() {
-    const formData = {
-        email: emailInput.value,
-        message: messageTextarea.value,
-    };
-    localStorage.setItem(LC_KEY, JSON.stringify(formData));
+feedbackForm.addEventListener('input', throttle(handleSaveToLS, 500));
+
+function handleSaveToLS() {
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+
+  localStorage.setItem(LC_KEY, JSON.stringify(formData));
 }
 
 // Loading data from local storage
 function loadFormState() {
-    const savedFormData = localStorage.getItem(LC_KEY);
-    if (savedFormData) {
-        const formData = JSON.parse(savedFormData);
-        emailInput.value = formData.email;
-        messageTextarea.value = formData.message;
-    }
-};
+  const savedFormData = localStorage.getItem(LC_KEY);
+  if (savedFormData) {
+    const formData = JSON.parse(savedFormData);
+    emailInput.value = formData.email || '';
+    messageTextarea.value = formData.message || '';
+  }
+}
 
-emailInput.addEventListener('input', saveFormState);
-messageTextarea.addEventListener('input', saveFormState);
+emailInput.addEventListener('input', () => {
+  handleSaveToLS();
+});
 
-loadFormState();
+messageTextarea.addEventListener('input', () => {
+  handleSaveToLS();
+});
+
+feedbackForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  localStorage.clear(LC_KEY);
+
+  emailInput.value = '';
+  messageTextarea.value = '';
+
+  const formDataObject = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  console.log(formDataObject);
+}
